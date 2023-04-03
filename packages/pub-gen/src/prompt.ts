2f68bfe0ebb6ee343e63cbe31802e357f3352e24
hgodinho <henrique@hgod.in>
2023-04-03 01:20:34 -0300

    feat(pub-gen): pacote gerador de publicações

---
+++ packages/pub-gen/src/prompt.ts
@@ -0,0 +1,74 @@
+export const pubGenPrompt = (args: Record<string, any>) => [
+    {
+        type: "input",
+        name: "titulo",
+        message: "Qual o título da publicação?",
+        default: args.titulo,
+    },
+    {
+        type: "list",
+        name: "tipo",
+        message: "Qual o tipo da publicação?",
+        default: args.tipo,
+        choices: ["artigo", "livro", "capítulo", "periódico", "outro"],
+    },
+    {
+        type: "input",
+        name: "instituicaoAfiliada",
+        message: "Qual a instituição afiliada?",
+        default: args.instituicaoAfiliada,
+    },
+    {
+        type: "input",
+        name: "instituicaoPublicacao",
+        message: "Qual a instituição da publicação?",
+        default: args.instituicaoPublicacao,
+    },
+    {
+        type: "input",
+        name: "ano",
+        message: "Qual o ano?",
+        default: args.ano,
+    },
+    {
+        type: "input",
+        name: "palavrasChave",
+        message: "Palavras chave separadas por vírgula",
+        default: args.palavrasChave,
+    },
+    {
+        type: "list",
+        name: "preset",
+        message: "Qual preset deseja usar?",
+        default: args.preset,
+        choices: ["abnt"],
+    },
+    {
+        type: "confirm",
+        name: "multiIdioma",
+        message: "A publicação será em mais de um idioma?",
+        default: args.multiIdioma,
+    },
+    {
+        type: "input",
+        name: "idiomas",
+        message:
+            "Quais os idiomas? Utilize a ISO 639-1, por exemplo: pt, en, es. Separe por vírgula",
+        default: args.idiomas,
+        when: (answers: Record<string, any>) => answers.multiIdioma,
+    },
+    {
+        type: "input",
+        name: "idiomaPadrao",
+        message:
+            "Qual o idioma padrão? Utilize a ISO 639-1, por exemplo: pt, en, es",
+        default: args.idiomaPrincipal,
+        when: (answers: Record<string, any>) => !answers.multiIdioma,
+    },
+    {
+        type: "confirm",
+        name: "instalarDependencias",
+        message: "Instalar dependências?",
+        default: args.instalarDependencias,
+    },
+];
