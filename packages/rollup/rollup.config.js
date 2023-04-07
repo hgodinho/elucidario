02dca094fd9591b85ca2847db5ea0283072f53c6
hgodinho <henrique@hgod.in>
2023-04-06 21:22:56 -0300

    feat(rollup): rollup config passa a ser function e recebe config

--- packages/rollup/rollup.config.js
+++ packages/rollup/rollup.config.js
@@ -1,29 +1,34 @@
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
+import merge from "lodash.merge";
 
-const lcdrRollupConfig = {
-    input: 'src/index.ts',
-    output: {
-        dir: "dist",
-        format: "es",
-        sourcemap: true,
-    },
-    plugins: [
-        typescript({
-            tsconfig: "tsconfig.json",
-        }),
-        terser(),
-    ],
-    external: [
-        "@elucidario/parse-args",
-        "fs",
-        "path",
-        "inquirer",
-        "child_process",
-        "lodash",
-        "json-schema-to-typescript",
-        "commander"
-    ]
+const lcdrRollupConfig = (config = null) => {
+    const defaultConfig = {
+        input: 'src/index.ts',
+        output: {
+            dir: "dist",
+            format: "es",
+            sourcemap: true,
+        },
+        plugins: [
+            typescript({
+                tsconfig: "tsconfig.json",
+            }),
+            terser(),
+        ],
+        external: [
+            "@elucidario/parse-args",
+            "fs",
+            "path",
+            "inquirer",
+            "child_process",
+            "lodash",
+            "json-schema-to-typescript",
+            "commander"
+        ]
+    }
+
+    return merge({}, defaultConfig, config);
 };
 
 export default lcdrRollupConfig;
