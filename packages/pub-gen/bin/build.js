fd34f0788d680136b07cf5a9798a6abd2b5222cf
hgodinho <henrique@hgod.in>
2023-04-19 01:19:26 -0300

    feat(pub-gen): add console.logs

--- packages/pub-gen/bin/build.js
+++ packages/pub-gen/bin/build.js
@@ -4,15 +4,16 @@ import path from "path";
 import fs from "fs";
 import { pubGenRemarkProcessor } from "../lib/remark/processor.js";
 import { readContents } from "@elucidario/pkg-schema-doc";
+import chalk from "chalk";
 
 const srcPath = path.resolve("src");
 const libPath = path.resolve("lib");
 const docsPath = path.resolve("docs");
 
-// console.log({ srcPath, libPath, docsPath });
 const docs = readContents(srcPath, ["md"]);
 
-Object.entries(docs).map(async ([name, content]) => {
+console.log(chalk.green("[pub-gen] Building docs..."));
+const build = Object.entries(docs).map(async ([name, content]) => {
     const newFile = await pubGenRemarkProcessor(content, {
         pubGen: {
             publication: "test",
@@ -21,4 +22,12 @@ Object.entries(docs).map(async ([name, content]) => {
     });
 
     fs.writeFileSync(path.resolve(docsPath, `${name}.md`), newFile.toString());
+    return `${name}.md`;
+});
+
+Promise.all(build).then((files) => {
+    console.log(
+        chalk.blueBright("[pub-gen] Built files:"),
+        chalk.blueBright(files)
+    );
 });
