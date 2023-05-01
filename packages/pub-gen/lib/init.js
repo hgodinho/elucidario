5f8cbc8630a1c8ba33bbbb25d086ae1455905238
hgodinho <henrique@hgod.in>
2023-05-01 02:39:05 -0300

    feat(pub-gen): init script para criar pub-gen-config.json

---
+++ packages/pub-gen/lib/init.js
@@ -0,0 +1,75 @@
+import inquirer from "inquirer";
+import fs from "fs";
+import path from "path";
+import { Console } from "@elucidario/pkg-console";
+
+import packageJson from "../package.json" assert { type: "json" };
+
+const console = new Console(packageJson);
+
+export const init = async (options) => {
+    const configPath = path.resolve(process.cwd(), "pub-gen-config.json");
+    if (options.force) {
+        buildInit(configPath);
+        return;
+    }
+    if (options.default) {
+        buildInit(configPath, true);
+        return;
+    }
+    if (fs.existsSync(configPath)) {
+        console.log("config file exists");
+        inquirer
+            .prompt([
+                {
+                    type: "confirm",
+                    name: "overwrite",
+                    message: "Overwrite existing config file?",
+                },
+            ])
+            .then((answers) => {
+                if (answers.overwrite) {
+                    buildInit(configPath);
+                }
+            });
+    } else {
+        buildInit(configPath);
+    }
+};
+
+export const buildInit = async (path, def) => {
+    if (def) {
+        fs.writeFileSync(
+            path,
+            JSON.stringify(
+                {
+                    reference: "references",
+                    publications: "publications",
+                },
+                null,
+                4
+            )
+        );
+        console.log(`Config file created at ${path}`);
+        return;
+    }
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "reference",
+                message: "What is the path to the references folder?",
+                default: "references",
+            },
+            {
+                type: "input",
+                name: "publications",
+                message: "What is the path to the publications folder?",
+                default: "publications",
+            },
+        ])
+        .then((answers) => {
+            fs.writeFileSync(path, JSON.stringify(answers, null, 4));
+            console.log(`Config file created at ${path}`);
+        });
+};
