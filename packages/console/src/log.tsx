cbac12a956a0ef9dc1baa49aed426a92b305cc94
hgodinho <henrique@hgod.in>
2023-05-02 03:04:34 -0300

    fix(console): melhoria do método log

--- packages/console/src/log.tsx
+++ packages/console/src/log.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Text, Newline, render} from 'ink';
+import chalk from 'chalk';
+
+type LogType = 'error' | 'warning' | 'info' | 'success';
 
 type LogProps = {
 	message: string;
@@ -8,11 +11,59 @@ type LogProps = {
 };
 import {PackageProps, Package} from './package-json.js';
 
+export type LogOptions = {
+	type?: LogType;
+	title?: string;
+	defaultLog?: boolean;
+};
+
 export class Console {
 	schema: PackageProps;
 
 	constructor(schema: PackageProps) {
 		this.schema = schema;
+	}
+
+	log(
+		message: string | any,
+		options: LogOptions = {type: 'info', defaultLog: false},
+	) {
+		let color = chalk.cyan;
+		let bg = chalk.bgCyanBright;
+		const {type, defaultLog, title} = options;
+		if (type) {
+			switch (type) {
+				case 'error':
+					color = chalk.red;
+					bg = chalk.bgRedBright;
+					break;
+				case 'warning':
+					color = chalk.yellow;
+					bg = chalk.bgYellowBright;
+					break;
+				case 'info':
+					color = chalk.cyan;
+					bg = chalk.bgCyanBright;
+					break;
+				case 'success':
+					color = chalk.green;
+					bg = chalk.bgGreenBright;
+					break;
+			}
+		}
+
+		if (defaultLog) {
+			console.log(bg(chalk.black(chalk.bold(` ${this.schema.name} `))));
+			if (title) console.log(color(`--- ${title} ---`));
+			console.log(message);
+			console.log(color(`---`));
+			return;
+		}
+
+		console.log(
+			bg(chalk.black(chalk.bold(` ${this.schema.name} `))),
+			color(message),
+		);
 	}
 
 	banner = () => {
