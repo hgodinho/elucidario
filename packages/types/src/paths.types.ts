9df1a35d15a0a1232bb85f326aa6b5b33afcc8ac
hgodinho <henrique@hgod.in>
2023-05-26 01:45:49 -0300

    feat(types): new types

--- packages/types/src/paths.types.ts
+++ packages/types/src/paths.types.ts
@@ -1,5 +1,8 @@
+import { PackageProps } from "./package.types";
+
 export type GetPathsReturn = {
     root: string;
+    current: string;
     packages: string;
     apps: string;
     publications: string;
@@ -9,4 +12,24 @@ export type GetPathsReturn = {
         apps: [string, string][];
         publications: [string, string][];
     }
+}
+
+export interface BuildOptions {
+    package: PackageProps;
+    program?: [
+        string,
+        string | undefined,
+        string | boolean | string[] | undefined
+    ][];
+    watchSrc?: string;
+}
+
+export interface BuildCallbackProps<T> {
+    programOptions: T;
+    options: BuildOptions;
+}
+
+export interface FNCallbackProps {
+    event: string;
+    filename?: string;
 }
