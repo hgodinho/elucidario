7459968427ce1034041a296a0e65c78033358423
hgodinho <henrique@hgod.in>
2023-05-26 18:25:17 -0300

    fix(paths): build
    remove commander

--- packages/paths/src/build.ts
+++ packages/paths/src/build.ts
@@ -1,4 +1,3 @@
-import { Command, OptionValues } from "commander";
 import { debounce } from "lodash-es";
 
 import type { BuildOptions, BuildCallbackProps, FNCallbackProps } from "@elucidario/pkg-types";
@@ -12,24 +11,10 @@ const { packages } = getPaths();
 
 export const build = async (
     options: BuildOptions,
-    callback: (props: BuildCallbackProps<OptionValues>) => Promise<void>
+    callback: (props: BuildCallbackProps) => Promise<void>
 ) => {
     try {
         if (!callback) throw new Error("callback is required");
-
-        const program = new Command("build");
-
-        program.option("-w, --watch", "Watch for changes", false);
-
-        if (options.program) {
-            options.program.forEach((option) => {
-                program.option(...option);
-            });
-        }
-
-        program.parse();
-
-        const programOptions = program.opts();
 
         const console = new Console(options.package);
 
@@ -38,34 +23,54 @@ export const build = async (
                 console.log(`${event}${filename ? `: ${filename}` : ""}`, {
                     type: "info",
                 });
-                await callback({ programOptions, options });
-            } catch (error: any) {
-                throw new Error(error)
+                await callback({ options });
+            } catch (err: any) {
+                throw new Error(err)
             }
         };
 
         await fn({ event: "Building..." });
 
-        if (programOptions.watch) {
+        if (options.watch) {
             if (!options.watchSrc) throw new Error("watchSrc is required");
 
             console.log("Watching for changes...", { type: "info" });
-            fs.watch(
-                path.resolve(options.watchSrc),
-                { recursive: true },
-                debounce(
-                    async (event: string, filename: string) => await fn({ event, filename }),
-                    500
-                )
-            );
+            try {
+                switch (typeof options.watchSrc) {
+                    case "string":
+                        fs.watch(
+                            path.resolve(options.watchSrc),
+                            { recursive: true },
+                            debounce(
+                                async (event: string, filename: string) => await fn({ event, filename }),
+                                500
+                            )
+                        );
+                        break;
+
+                    case "object":
+                        options.watchSrc.forEach((src: string) => {
+                            fs.watch(
+                                path.resolve(src),
+                                { recursive: true },
+                                debounce(
+                                    async (event: string, filename: string) => await fn({ event, filename }),
+                                    500
+                                )
+                            );
+                        });
+                }
+            } catch (err: any) {
+                throw new Error(err);
+            }
         }
     }
 
-    catch (error: any) {
+    catch (err: any) {
         const packageJson = JSON.parse(fs.readFileSync(
             path.resolve(packages, 'paths', "package.json")
         ).toString());
         const console = new Console(packageJson);
-        console.log(error, { type: "error", defaultLog: true, title: "Error" });
+        console.log(err, { type: "error", defaultLog: true, title: "Error" });
     }
 };
