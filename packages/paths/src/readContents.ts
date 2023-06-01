7c593147fe6b474a12ea4551b065282d47b9398c
hgodinho <henrique@hgod.in>
2023-06-01 00:54:17 -0300

    fix(paths): corrige readContents

--- packages/paths/src/readContents.ts
+++ packages/paths/src/readContents.ts
@@ -1,21 +1,10 @@
 import fs from "fs";
 import path from "path";
-import type { PackageProps } from "@elucidario/pkg-types";
+
+import type { PackageProps, ReadContentsProps, ReadContentsReturn } from "@elucidario/pkg-types";
 
 import { getPaths } from "./getPaths";
 import { Console } from "@elucidario/pkg-console";
-
-type ReadContentsReturn = { [key: string]: string | any };
-
-type ReadContentsProps = {
-    dirPath: string;
-    index?: boolean;
-    extensions?: string[];
-    names?: boolean;
-    exclude?: string[];
-    log?: boolean;
-    package?: PackageProps;
-};
 
 const imagesExtensions = [
     "jpg",
@@ -28,22 +17,59 @@ const imagesExtensions = [
     "ico",
 ];
 
+export const readFile = (filePath: string, ext: string): ReadContentsReturn | string => {
+    try {
+        switch (ext) {
+            case 'json':
+                return JSON.parse(
+                    fs.readFileSync(
+                        path.resolve(filePath),
+                        "utf-8"
+                    ).toString()
+                );
+
+            case 'md':
+                return fs.readFileSync(
+                    path.resolve(filePath),
+                    "utf-8"
+                ).toString();
+
+            default:
+                return fs.readFileSync(
+                    path.resolve(filePath),
+                ).toString();
+        }
+    } catch (err: any) {
+        throw new Error(`Cannot read file: ${filePath}`);
+    }
+}
+
 /**
- * Lê o conteúdo de um diretório e retorna um objeto com o conteúdo dos arquivos ou um array com os nomes dos arquivos
- * @param {ReadContentsProps} props
- * @returns {object|Array} Objeto com o conteúdo dos arquivos
+ * Lê o conteúdo de um diretório recursivamente, retornando um objeto com o conteúdo de cada arquivo ou path
+ * 
+ * @param {Object} props - Propriedades
+ * @param {string} props.dirPath - Path do diretório a ser lido
+ * @param {boolean} [props.index=true] - Se deve ler o index.json ou não
+ * @param {string[]} [props.extensions=["json", "md"]] - Extensões dos arquivos a serem lidos
+ * @param {string} [props.returnType="content"] - Tipo de retorno: "content" ou "path"
+ * @param {string[]} [props.exclude] - Arquivos a serem excluídos
+ * @param {boolean} [props.log=false] - Se deve logar no console ou não
+ * @param {PackageProps} [props.package] - Objeto package.json
+ * 
+ * @returns {Object} - Objeto com o conteúdo de cada arquivo ou path
  */
 export const readContents = ({
     dirPath,
     index = true,
     extensions = ["json", "md"],
-    names = false,
+    returnType = 'content',
     exclude,
     log,
     package: pkg,
 }: ReadContentsProps): ReadContentsReturn => {
     const result: ReadContentsReturn = {};
 
+    // Selecionar o package.json do projeto, caso não seja recebido como parâmetro
     if (!pkg) {
         pkg = JSON.parse(
             fs.readFileSync(
@@ -55,138 +81,94 @@ export const readContents = ({
 
     const console = new Console(pkg as PackageProps);
 
-    // ler todos os arquivos na pasta atual
-    let files: string[] = [];
-
-    try {
-        if (index && fs.existsSync(path.resolve(dirPath, "index.json"))) {
-            const index = JSON.parse(
-                fs.readFileSync(path.resolve(dirPath, "index.json"), "utf-8")
-            );
-            files = index.files.map((file: string) => {
-                if (fs.existsSync(path.resolve(dirPath, `${file}.md`))) {
-                    return `${file}.md`;
-                } else {
-                    return file;
-                }
-            });
-        } else {
-            files = fs.readdirSync(dirPath);
-        }
-    } catch (err) {
-        console.log(
-            { files, err, },
-            {
-                defaultLog: true,
-                type: "error",
-                title: "readContents",
-            }
-        );
-    }
-
-    if (!files) {
+    // caso index seja false lê o diretório
+    if (!index) {
         try {
-            files = fs.readdirSync(dirPath);
-        } catch (err) {
-            console.log(
-                { err },
-                {
-                    defaultLog: true,
-                    type: "error",
-                    title: "readContents",
-                }
-            );
-        }
-    }
+            const files = fs.readdirSync(dirPath);
 
-    // iterar sobre cada arquivo encontrado
-    const ignored: string[] = [];
 
-    try {
-        files.forEach((file: string) => {
-            const filePath = path.join(dirPath, file);
+            files.forEach((file: string) => {
+                const filePath = path.parse(file);
 
-            if (exclude && exclude.includes(file)) {
-                ignored.push(filePath);
-                return;
-            }
-
-            // se o arquivo for um diretório, ler seu conteúdo recursivamente
-            if (fs.statSync(filePath).isDirectory()) {
-                if (names) {
-                    result[file] = Object.entries(
-                        readContents({ dirPath: filePath, extensions, names, log, exclude, package: pkg })
-                    ).map(([key, value]) => key);
-                } else {
-                    result[file] = readContents({
-                        dirPath: filePath,
+                // se for um diretório, chama a função recursivamente
+                if (filePath.ext === "") {
+                    result[filePath.name] = readContents({
+                        dirPath: path.resolve(dirPath, file),
+                        index,
                         extensions,
-                        names,
+                        returnType,
                         exclude,
                         log,
                         package: pkg,
                     });
                 }
-            } else {
-                // verifica o tipo de arquivo
-                const file = path.parse(filePath);
-                const ext = file.ext.replace(".", "");
 
-                if (extensions.includes(ext) && ext === "json") {
-                    if (names) {
-                        result[file.name] = filePath;
-                    } else {
-                        const fileContent = JSON.parse(
-                            fs.readFileSync(filePath, "utf-8")
-                        );
-
-                        result[file.name] = fileContent;
-                    }
-                } else if (extensions.includes(ext) && ext === "md") {
-                    if (names) {
-                        result[file.name] = filePath;
-                    } else {
-                        const fileContent = fs.readFileSync(filePath, "utf-8");
-                        result[file.name] = fileContent;
-                    }
-                } else if (
-                    extensions.includes(ext) &&
-                    imagesExtensions.includes(ext)
-                ) {
-                    // é uma imagem, deve retornar sempre somente o path, ao invés do conteúdo
-                    if (names) {
-                        result[file.name] = filePath;
-                    } else {
-                        throw new Error(
-                            "Não é possível ler o conteúdo de uma imagem, defina names como true"
-                        );
+                // se for um arquivo, lê o conteúdo
+                const ext = filePath.ext.replace(".", "");
+                if (extensions.includes(ext)) {
+                    switch (returnType) {
+                        case 'path':
+                            result[filePath.name] = path.resolve(dirPath, file);
+                            break;
+                        case 'content':
+                            result[filePath.name] = readFile(path.resolve(dirPath, file), ext);
                     }
                 }
-            }
-        });
-    } catch (err) {
-        console.log(
-            { err },
-            {
-                defaultLog: true,
-                type: "error",
-                title: "readContents",
-            }
-        );
-    }
-
-    if (typeof log === "undefined") {
-        log = true;
-    }
-    if (log) {
-        if (ignored.length > 0) {
-            console.log(ignored, {
-                defaultLog: true,
-                type: "warning",
-                title: "Ignored files",
             });
+        } catch (err: any) {
+            throw new Error(`Cannot read directory: ${dirPath}`);
         }
     }
 
+    // caso index seja true, lê o index.json
+    else {
+        try {
+            const indexJson = JSON.parse(
+                fs.readFileSync(path.resolve(dirPath, "index.json"), "utf-8")
+            );
+
+            indexJson.files.forEach((file: string) => {
+                const filePath = path.parse(file);
+
+                // se for um diretório, chama a função recursivamente
+                if (filePath.ext === "") {
+                    const content = readContents({
+                        dirPath: path.resolve(dirPath, file),
+                        index,
+                        extensions,
+                        returnType,
+                        exclude,
+                        log,
+                        package: pkg,
+                    });
+                    result[filePath.name] = content;
+                } else {
+                    // se for um arquivo, lê o conteúdo
+                    const ext = filePath.ext.replace(".", "");
+                    if (extensions.includes(ext)) {
+
+                        switch (returnType) {
+                            case 'path':
+                                result[filePath.name] = path.resolve(dirPath, file);
+                                break;
+                            case 'content':
+                                result[filePath.name] = readFile(path.resolve(dirPath, file), ext);
+                        }
+                    }
+                }
+            });
+        } catch {
+            throw new Error(`Cannot read index.json in directory: ${dirPath}`);
+        }
+    }
+
+    // caso seja passado um array de arquivos a serem ignorados, remove-os do resultado
+    if (exclude) {
+        exclude.forEach((file: string) => {
+            delete result[file];
+        });
+    }
+
     return result;
+
 };
