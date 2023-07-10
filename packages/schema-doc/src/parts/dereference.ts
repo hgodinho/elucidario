0f32725a306e0b6afe7ca1ff179a3df3583f725b
hgodinho <henrique@hgod.in>
2023-07-10 00:02:00 -0300

    feat(schema-doc): update deps + dereference + bundle

---
+++ packages/schema-doc/src/parts/dereference.ts
@@ -0,0 +1,18 @@
+import $RefParser from "@apidevtools/json-schema-ref-parser";
+import { JSONSchema7 } from "json-schema";
+import type $RefParserOptions from "@apidevtools/json-schema-ref-parser/dist/lib/options";
+
+export const dereference = async (
+    schema: JSONSchema7,
+    options?: $RefParserOptions,
+): Promise<JSONSchema7 | Error> => {
+    try {
+        schema = await $RefParser.dereference(
+            schema,
+            options as $RefParserOptions
+        ) as JSONSchema7;
+    } catch (e: unknown) {
+        console.log(e);
+    }
+    return schema;
+};
