f6da985cc9bad44e7774dea5f6541480534d9b61
hgodinho <henrique@hgod.in>
2023-07-11 00:44:33 -0300

    feat(mdorim): type

---
+++ packages/mdorim/lib/types.js
@@ -0,0 +1,15 @@
+/**
+ * MDORIM type definition
+ * @typedef {Object} MDORIM
+ * @property {string} name - Name of the MDORIM
+ * @property {string} version - Version of the MDORIM
+ * @property {string} description - Description of the MDORIM
+ * @property {Record<string, any>} schemas - Schemas of the MDORIM
+ * @property {Record<string, any>} translations - Translations of the MDORIM
+ * @property {Record<string, any>} mapping - Mapping of the MDORIM
+ * @property {Record<string, any>} examples - Examples of the MDORIM
+ * @property {function} dereference - Dereference function
+ * @property {function} validate - Validate function
+ */
+
+export { MDORIM };
