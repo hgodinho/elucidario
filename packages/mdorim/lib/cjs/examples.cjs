05b52b17041b1fdbe34827474cfeb68fadfc94eb
hgodinho <henrique@hgod.in>
2023-07-11 00:25:38 -0300

    refactor(mdorim): cjs export

---
+++ packages/mdorim/lib/cjs/examples.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    rest: {
+        object1: require("../../static/mdorim/examples/rest/object-1.json"),
+    },
+    storage: {
+        object: require("../../static/mdorim/examples/storage/object.json"),
+    },
+};
