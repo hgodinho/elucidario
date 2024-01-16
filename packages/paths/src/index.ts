dc574d099b86e460244da5cd1edfdfeef435a6b1
hgodinho <henrique@hgod.in>
2024-01-16 01:04:29 -0300

    feat(funcoes assincronas): pub-gen assincrono

--- packages/paths/src/index.ts
+++ packages/paths/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./getPaths";
 export * from "./readContents";
+export * from "./asyncReadContents";
 export * from "./build";
 export * from "./file";
+export * from "./asyncFile";
 export * from "./folder";
