d9416712fda174430960d11ffee2abfce624e81e
hgodinho <henrique@hgod.in>
2024-02-17 11:06:23 -0300

    fix(parse-args): adiciona dist ao gitignore e deleta arquivos dist do git

--- tools/parse-args/dist/index.js
+++
@@ -1,2 +0,0 @@
-const s=()=>{const s={path:""};return process.argv.forEach((t=>{if(t.startsWith("--")){const[e,i]=t.slice(2).split("=");s[e]=i||!0}else t.includes("node_modules")?s.path=t.split("node_modules")[0]:t.includes("dist")&&(s.path=t.split("dist")[0])})),s};export{s as parseArgs};
-//# sourceMappingURL=index.js.map
--- tools/parse-args/dist/parse-args.d.ts
+++
@@ -1,6 +0,0 @@
-interface ParsedArgs {
-    path: string;
-    [key: string]: string | boolean;
-}
-export declare const parseArgs: () => ParsedArgs;
-export {};
